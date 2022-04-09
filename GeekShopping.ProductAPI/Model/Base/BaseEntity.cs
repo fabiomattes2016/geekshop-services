627d5f91fe495485c7f5cf8431f536d61cfba0d2
Fabio Mattes <fabiomattes2007@gmail.com>
2022-04-09 12:47:22 -0300

    Primeiro Commit

---
+++ GeekShopping.ProductAPI/Model/Base/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProductAPI.Model.Base
+{
+    public class BaseEntity
+    {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
---
+++ GeekShopping.ProductAPI/Model/Product.cs
@@ -0,0 +1,32 @@
+﻿using GeekShopping.ProductAPI.Model.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProductAPI.Model
+{
+    [Table("products")]
+    public class Product : BaseEntity
+    {
+        [Column("name")]
+        [Required(ErrorMessage = "Campo nome é obrigatório")]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Column("price")]
+        [Required(ErrorMessage = "Campo preço é obrigatório")]
+        [Range(1, 1000000)]
+        public decimal Price { get; set; }
+
+        [Column("description")]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Column("category_name")]
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        [Column("image_url")]
+        [StringLength(300)]
+        public string ImageUrl { get; set; }
+    }
+}
